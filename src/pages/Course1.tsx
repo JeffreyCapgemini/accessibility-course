08a92b4f6849b1234019c252c2cfd3abb1a909ab
Jeffrey Schuurman <jeffrey.schuurman@capgemini.com>
2026-02-13 14:21:52 +0100

    added navigation

---
+++ src/pages/Course1.tsx
@@ -0,0 +1,10 @@
+const Course1 = () => {
+  return (
+    <div>
+      <h1>Assigment 1</h1>
+      <p>Dit is de course1pagina.</p>
+    </div>
+  );
+};
+
+export default Course1;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Dit is de home pagina.</p>
+    </div>
+  );
+};
+
+export default Home;
